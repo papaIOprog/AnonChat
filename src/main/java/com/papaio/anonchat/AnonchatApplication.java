3a2b9246ed80a611fc3f9ed29919545476d864a1
Vadim Baryshev <vadim.baryshev@bell-sw.com>
2021-09-12 15:47:31 +0300

    Initial commit

---
+++ src/main/java/com/papaio/anonchat/AnonchatApplication.java
@@ -0,0 +1,13 @@
+package com.papaio.anonchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnonchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnonchatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/papaio/anonchat/AnonchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.papaio.anonchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnonchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
